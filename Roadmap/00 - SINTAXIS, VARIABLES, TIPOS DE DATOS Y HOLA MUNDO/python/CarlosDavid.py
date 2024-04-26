5da4b1cae0d9ee7b6637f2c94af63f021fe31324
dinitrion <33130530+dinitrion@users.noreply.github.com>
2024-04-26 12:43:57 -0400

    Create CarlosDavid.py
    primer ejercicio, aprendiendo logica de programacion.

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CarlosDavid.py
@@ -0,0 +1,23 @@
+# aqui vamos a colocar el enlace del sitio oficial de python https://www.python.org/
+'''
+Esperemos que desde esa pagina
+poder sacar todas las 
+herramientas necesarias para aprender
+este lenguaje.
+'''
+"""
+y gracias a esta plataforma
+los creadores 
+y el contenido que crearon
+a que podamos entrar a este mundo de la programacion
+"""
+#aqui va un ejemplo de variable
+nombre = 'carlos'
+#tipos de variables primitivas
+edad = 25 # variable tipo entero (int)
+altura = 1,67 # variable tipo flotante (float) numeros con decimales
+mensaje = 'Hola, ¿cómo estás?' # cadena de texto (str)
+es_mayor_de_edad = True #Booleanos (bool) verdadero o falso
+
+#imprimir por pantalla
+print("hola Phyton")
