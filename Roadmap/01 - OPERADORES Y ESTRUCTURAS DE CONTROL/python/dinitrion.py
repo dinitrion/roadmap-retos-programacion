50a8796912921c4da4f0c9abaef4d2963920ca64
CarlosDavid <33130530+dinitrion@users.noreply.github.com>
2024-05-07 07:47:14 -0400

    Create dinitrion.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/dinitrion.py
@@ -0,0 +1,9 @@
+a = 10
+b = 4
+print(a + b)  # Suma: 14
+print(a - b)  # Resta: 6
+print(a * b)  # Multiplicación: 40
+print(a / b)  # División: 2.5
+print(a // b) # División entera: 2
+print(a % b)  # Módulo: 2
+print(a ** b) # Exponenciación: 2500
